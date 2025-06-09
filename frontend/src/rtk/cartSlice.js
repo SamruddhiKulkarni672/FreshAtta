177f2613a1ce7fceebf8a3192f75edbcfc3adf75
poojalahane <poojalahane144@gmail.com>
2025-06-09 16:42:02 +0530

    cart page  updated

--- frontend/src/rtk/cartSlice.js
+++ frontend/src/rtk/cartSlice.js
@@ -37,7 +37,7 @@ const cartSlice = createSlice({
     },
     removeFromCart(state, action) {
       const id = action.payload;
-      const findItem = state.products.find((item) => item.id !== id);
+      const findItem = state.products.find((item) => item.id === id);
       if (findItem) {
         state.totalPrice -= findItem.totalPrice;
         state.products = state.products.filter((item) => item.id !== id);
